eea10b41900b76597615e4e35b5b9cb22e0ab2d4
Dinzeyi2 <133053311+Dinzeyi2@users.noreply.github.com>
2026-01-02 09:51:30 -0500

    Update __init__.py

--- app/demand_forecasting/__init__.py
+++ app/demand_forecasting/__init__.py
@@ -1,3 +1,3 @@
-from .algorithms_demand_forecasting import execute_demand_forecasting
+from algorithms_demand_forecasting import execute_demand_forecasting
 
 __all__ = ['execute_demand_forecasting']
